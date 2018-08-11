bb4ab6275da1e7ae91832a13b76942f2a0889d3c
Amy Frenzel <frenzel.amy@gmail.com>
2018-08-11 23:33:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+  
+function add(a, b) {
+ return a + b
+}
+
+function subtract(a, b){
+  return a - b
+}
+
+function multiply(a, b){
+  return a * b
+}
+
+function divide(a, b){
+  return a / b
+}
+
+function inc(n){
+  return n += 1
+}
+
+function dec(n){
+  return n -= 1
+}
+
+function makeInt(n){
+  parseInt('n', 10)
+  parseInt("0x2038", 10)
+  parseInt("sldkjflksjf", 10)
+}
